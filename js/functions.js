ebde1653ceacabaa82c17b6ec25eacc42d294f9c
swagathub <sinu2290@gmail.com>
2021-08-09 23:44:10 -0400

    Registration implementation.

---
+++ js/functions.js
@@ -0,0 +1,50 @@
+$(document).ready(function () {
+  var pcodeRegex = /^(\d{5}(-\d{4})?|\d[A-Z]\d ?[A-Z]\d[A-Z])$/,
+    phoneRegex = /^\(?\d{3}\)?[-]?\d{3}[\s.-]\d{4}$/;
+
+  let userInfo = JSON.parse(localStorage.getItem("userInfo")) || [];
+  if (userInfo.length > 0) {
+    $(".record").removeClass("dnone");
+  }
+
+  let formValidation = function () {
+    let flag = true;
+    $(".form-field").each((index, item) => {
+      if ($(item).val() == null || $(item).val() === "") {
+        alert("Please fill all the fields!");
+        flag = false;
+        return false;
+      }
+    });
+    if (flag) {
+      if (!pcodeRegex.test($(".container .form input[name='pcode']").val())) {
+        flag = false;
+        alert(
+          "Postal Code (Acceptable format: NAN ANA – where 'A' is any alphabetic character and 'N' is avalid numeric digit)"
+        );
+      }
+      if (!phoneRegex.test($(".container .form input[name='phone']").val())) {
+        flag = false;
+        alert("Acceptable formats: 123-123-1234, or (123)123-1234");
+      }
+      if (flag) {
+        var formData = $(".container .form").serializeArray();
+        var userInfo = JSON.parse(localStorage.getItem("userInfo")) || [];
+        var info = {};
+        formData.forEach((item) => {
+          info[item.name] = item.value;
+        });
+        userInfo.push(info);
+        localStorage.setItem("userInfo", JSON.stringify(userInfo));
+        $(".record").removeClass("dnone");
+        alert("Welcome! You are a member now.");
+      }
+    }
+  };
+
+  $("#register").on("click", function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+    formValidation();
+  });
+});
